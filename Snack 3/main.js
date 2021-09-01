16ffc1de9d1593ef6aca98988dda20c93255bbb0
Luciano <lucco6823@gmail.com>
2021-09-01 15:54:27 +0200

    Primo step completato

--- Snack 3/main.js
+++ Snack 3/main.js
@@ -1,3 +1,21 @@
 // Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
+
+const array = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+let a, b = 0;
+
+do{
+    a = numbersRandom(1, 10);
+    b = numbersRandom(1, 10);
+}while(b < a);
+
+console.log(array);
+console.log(`Il numero A generato casualmente è ${a}.
+Il numero B generato casualmente è ${b}.`);
+
+function numbersRandom(min, max){
+    return risultato = Math.floor(Math.random() * max - min + 1) + min; 
+}
+
 // La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
 // Usiamo i nuovi metodi degli array foreach o filter.
+
