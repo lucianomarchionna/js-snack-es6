22e8109357588c8d7b7271d19e3ea745c9771f08
Luciano <lucco6823@gmail.com>
2021-09-01 16:40:18 +0200

    Snack 3 Completato

--- Snack 3/main.js
+++ Snack 3/main.js
@@ -1,16 +1,14 @@
 // Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
 
-const array = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+const array = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
 let a, b = 0;
 
 do{
-    a = numbersRandom(1, 10);
-    b = numbersRandom(1, 10);
+    a = numbersRandom(0, 10);
+    b = numbersRandom(0, 10);
 }while(b < a);
 
 console.log(array);
-console.log(`Il numero A generato casualmente è ${a}.
-Il numero B generato casualmente è ${b}.`);
 
 function numbersRandom(min, max){
     return risultato = Math.floor(Math.random() * max - min + 1) + min; 
@@ -19,3 +17,11 @@ function numbersRandom(min, max){
 // La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
 // Usiamo i nuovi metodi degli array foreach o filter.
 
+function beetween (array, a, b){
+    return array.filter((element, index) => {
+        return a <= index && b >= index;
+    });
+};
+
+let numberBeetween = beetween(array, a, b);
+console.log(`I numeri che hanno la posizione compresa tra ${a} e ${b} sono ${numberBeetween}`);
